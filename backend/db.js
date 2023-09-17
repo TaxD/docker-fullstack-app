1c2681a5ce22ab4595cc75dcda426c06151810fa
TaxD <88539690+TaxD@users.noreply.github.com>
2023-09-17 20:33:49 +0900

    asd

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: '',
-    user: '',
-    password: '',
-    database: '',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
